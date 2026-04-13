49d87a4b2691e31b1537a82e24e31636be1a8353
MoloyKumarDas <moloykumardas10@gmail.com>
2026-04-13 19:55:32 +0600

    before learning bean,Added Razorpay payment service and run/test it at application startup using CommandLineRunner

---
+++ src/main/java/com/example/SpringBootLearning1/RazorpayPaymentService.java
@@ -0,0 +1,10 @@
+package com.example.SpringBootLearning1;
+
+public class RazorpayPaymentService {
+    public String pay(){
+        String payment="Razorpay Payment";
+        System.out.println("Payment from: "+payment);
+        return payment;
+    }
+
+}
--- src/main/java/com/example/SpringBootLearning1/SpringBootLearning1Application.java
+++ src/main/java/com/example/SpringBootLearning1/SpringBootLearning1Application.java
@@ -5,10 +5,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootLearning1Application {
+public class SpringBootLearning1Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLearning1Application.class, args);
 	}
 
+	private RazorpayPaymentService paymentService=new RazorpayPaymentService();
+
+	@Override
+	public void run(String... args)throws Exception{
+		String payment=paymentService.pay();
+		System.out.println("Payment done: "+payment);
+
+}
 }
