908094c42b374038d3f91bdc03c83fe2c2a4a3dd
MoloyKumarDas <moloykumardas10@gmail.com>
2026-04-17 13:09:23 +0600

    adding 2 more dependency & also connecting with database

--- src/main/java/com/example/SpringBootLearning1/SpringBootLearning1Application.java
+++ src/main/java/com/example/SpringBootLearning1/SpringBootLearning1Application.java
@@ -7,25 +7,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
-public class SpringBootLearning1Application implements CommandLineRunner {
-//public class SpringBootLearning1Application {
+//public class SpringBootLearning1Application implements CommandLineRunner {
+public class SpringBootLearning1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLearning1Application.class, args);
 	}
 
 
-	private final PaymentService paymentService;
-
-	public SpringBootLearning1Application(PaymentService paymentService) {              // currently there are 2 bean // razor & stripe // but i have to choose 1
-		this.paymentService = paymentService;                                           // i have used application.properties & condition to select one bean
-	}
-
-	@Override
-	public void run(String... args)throws Exception{
-		String payment=paymentService.pay();
-		System.out.println("Payment done: "+payment);
-}
+//	private final PaymentService paymentService;
+//
+//	public SpringBootLearning1Application(PaymentService paymentService) {              // currently there are 2 bean // razor & stripe // but i have to choose 1
+//		this.paymentService = paymentService;                                           // i have used application.properties & condition to select one bean
+//	}
+//
+//	@Override
+//	public void run(String... args)throws Exception{
+//		String payment=paymentService.pay();
+//		System.out.println("Payment done: "+payment);
+//}
 
 
 }
